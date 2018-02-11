a7ef17492395e08d0da7cfd229d1db6eb192aae8
Aitor González Fernández <reimashi@gmail.com>
2018-02-11 17:52:13 +0100

    Use of shelf request and response simplified

--- example/crossroad_example.dart
+++ example/crossroad_example.dart
@@ -7,21 +7,21 @@ void main() {
   Crossroad.addMiddleware(shelf.logRequests());
 
   // Simple string path
-  Crossroad.get('/', (shelf.Request request, _) {
-    return new shelf.Response.ok('Hello world');
+  Crossroad.get('/', (Request request, _) {
+    return new Response.ok('Hello world');
   });
 
   // Regexp path
-  Crossroad.get(new RegExp(r"^/[0-9]{5}$"), (shelf.Request request, _) {
-    return new shelf.Response.ok("5-digit number: ${request.url}");
+  Crossroad.get(new RegExp(r"^/[0-9]{5}$"), (Request request, _) {
+    return new Response.ok("5-digit number: ${request.url}");
   });
 
   // Nested routers, for relative paths
   Router paramsRouter = Crossroad.route("/params");
 
   // Extract parameters from paths
-  paramsRouter.get('/:param1/:param2/:param3', (shelf.Request request, Map<String, Object> p) {
-    return new shelf.Response.ok("Parameters: ${p.toString()}");
+  paramsRouter.get('/:param1/:param2/:param3', (Request request, Map<String, Object> p) {
+    return new Response.ok("Parameters: ${p.toString()}");
   });
 
   // Shelf standard handler
--- lib/crossroad.dart
+++ lib/crossroad.dart
@@ -3,3 +3,6 @@ library crossroad;
 
 export 'src/router.dart';
 export 'src/router_handler.dart';
+
+export 'package:shelf/src/request.dart';
+export 'package:shelf/src/response.dart';
